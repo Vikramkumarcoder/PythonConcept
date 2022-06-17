3bbac30e4515c27117ed202a8ac0df5f5c413c7f
Vikramkumarcoder <104130605+Vikramkumarcoder@users.noreply.github.com>
2022-06-17 12:16:08 +0530

    Add files via upload

---
+++ build-in function.py
@@ -0,0 +1,13 @@
+var1= 'My name is Vikram Kumar , 16'
+var2='Mynameisrohan16'
+#print(len(var1[-4:-2]))
+#print(var1 [:])
+print(var1 . capitalize())
+print(var1 . count('a'))
+print(var1 . endswith('kuma'))
+print(var1 . lower())
+print(var1 . upper())
+print(var2 . isalnum())
+print(var2 . isalpha())
+print(var1 . find('name'))
+print(var1 . replace('name' , 'fame'))
