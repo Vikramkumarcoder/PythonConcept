011824fad35e7298a012113de9988e7ecb1d5917
Vikram Kumar <104130605+Vikramkumarcoder@users.noreply.github.com>
2022-06-26 23:25:08 +0530

    Add files via upload

---
+++ BasicConceps/short hand if and else.py
@@ -0,0 +1,4 @@
+a = int(input ('Enter first number\n')) 
+b = int(input ('Enter second number\n'))
+#if a>b:	print("a is greater than b")
+print ("b is greater than a") if a<b else print("a is greater than b")
